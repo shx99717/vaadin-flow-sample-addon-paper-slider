1e28f83f7e2d507c406fcb179eebf6c2a7bafa36
Raymond Shen <raymond.shen@medavis.com>
2021-09-26 16:47:15 +0800

    new content added

---
+++ src/test/java/org/vaadin/addons/View.java
@@ -0,0 +1,13 @@
+package org.vaadin.addons;
+
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.router.Route;
+
+@Route("")
+public class View extends Div {
+
+    public View() {
+        PaperSlider paperSlider = new PaperSlider();
+        add(paperSlider);
+    }
+}
