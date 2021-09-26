eb80ebf06fe370de588dc3c357da1938b574b194
Raymond <raymond.shen@medavis.com>
2021-09-26 22:52:28 +0800

    new content added

--- src/main/java/org/vaadin/addons/PaperSlider.java
+++ src/main/java/org/vaadin/addons/PaperSlider.java
@@ -15,10 +15,13 @@ import com.vaadin.flow.component.dependency.NpmPackage;
 //• inside META-INF/resources/frontend directory (inside a projectresources folder) in case of JAR project (if you are using Maven this is src/main/resources/META-INF/resources/frontend directory). 
 //It is guaranteed that dependencies will be loaded only once. The files loadedwill be in the same order as the annotations were on the class. However,loading order is only guaranteed on a class level; Annotations from differentclasses may appear in different order, grouped by the annotated class. Also,files identified by @JsModule will be loaded before com.vaadin.flow.component.dependency.JavaScript and com.vaadin.flow.component.dependency.CssImport. 
 @JsModule("@polymer/paper-slider/paper-slider.js")
-// This is the extra file being loaded when a paper-slide added to the page
-//  If you wish to include your own JS modules in the add-on jar, add the module
-//  files to './src/main/resources/META-INF/resources/frontend' and insert an
-//  annotation @JsModule("./my-module.js") here. 
+// This is the extra js file being added
+// this js file will be finally included in an all-in-one bundle.js/main.js after webpack, therefore even the referencing
+// component in this case the PaperSlider has not been initialized on the page, this javascript will
+// still be triggered, in this sense, this is global included
+// If you wish to include your own JS modules in the add-on jar, add the module
+// files to './src/main/resources/META-INF/resources/frontend' and insert an
+// annotation @JsModule("./my-module.js") here. 
 @JsModule("./my-module.js")
 
 //Annotation for defining an NPM package dependency on a Componentclass which includes one or more JS modules. The JS Modules can be definedusing JsModule annotation on the same Component. For addingmultiple NPM packages files for a single component, you can use thisannotation multiple times. 
--- src/main/resources/META-INF/resources/frontend/my-module.js
+++ src/main/resources/META-INF/resources/frontend/my-module.js
@@ -1,1 +1,4 @@
-alert("The file my-module.js is loaded");
+// this js file will be finally included in an all-in-one bundle.js/main.js after webpack, therefore even the referencing
+// component in this case the PaperSlider has not been initialized on the page, this javascript will
+// still be triggered, in this sense, this is global included
+console.log("The file my-module.js is loaded");
