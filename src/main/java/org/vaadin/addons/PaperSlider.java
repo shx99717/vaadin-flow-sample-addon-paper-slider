4009a5504778b58c74dbdb04c78e2d0e93988870
Raymond <raymond.shen@medavis.com>
2022-01-14 00:50:39 +0800

    fix some typos

--- src/main/java/org/vaadin/addons/PaperSlider.java
+++ src/main/java/org/vaadin/addons/PaperSlider.java
@@ -16,14 +16,14 @@ import com.vaadin.flow.shared.Registration;
 //This defines the name of the HTML element to be presented on the page
 //This must be matched with the predefined name from the web-component
 @Tag("paper-slider")
-//Annotation for defining an NPM package dependency on a Componentclass which includes one or more JS modules. The JS Modules can be definedusing JsModule annotation on the same Component. For addingmultiple NPM packages files for a single component, you can use thisannotation multiple times. 
-//Declared NPM packages will be bundled by flow-maven-plugin in a package.jsonfile, making sure that only one dependency is created.
+//Annotation for defining a NPM package dependency on a Component class which includes one or more JS modules. The JS Modules can be defined using JsModule annotation on the same Component. For adding multiple NPM packages files for a single component, you can use this annotation multiple times. 
+//Declared NPM packages will be bundled by flow-maven-plugin in a package.json file, making sure that only one dependency is created.
 @NpmPackage(value = "@polymer/paper-slider", version = "3.0.1")
-//Annotation for defining JavaScript Module dependencies on a Componentclass. For adding multiple JavaScript Module files for a single component,you can use this annotation multiple times. 
+//Annotation for defining JavaScript Module dependencies on a Component class. For adding multiple JavaScript Module files for a single component,you can use this annotation multiple times. 
 //The JavaScript module files should be located: 
-//• inside frontend directory in your root project folder in case ofWAR project 
-//• inside META-INF/resources/frontend directory (inside a projectresources folder) in case of JAR project (if you are using Maven this is src/main/resources/META-INF/resources/frontend directory). 
-//It is guaranteed that dependencies will be loaded only once. The files loadedwill be in the same order as the annotations were on the class. However,loading order is only guaranteed on a class level; Annotations from differentclasses may appear in different order, grouped by the annotated class. Also,files identified by @JsModule will be loaded before com.vaadin.flow.component.dependency.JavaScript and com.vaadin.flow.component.dependency.CssImport. 
+//• inside frontend directory in your root project folder in case of WAR project 
+//• inside META-INF/resources/frontend directory (inside a project resources folder) in case of JAR project (if you are using Maven this is src/main/resources/META-INF/resources/frontend directory). 
+//It is guaranteed that dependencies will be loaded only once. The files loaded will be in the same order as the annotations were on the class. However,loading order is only guaranteed on a class level; Annotations from different classes may appear in different order, grouped by the annotated class. Also,files identified by @JsModule will be loaded before com.vaadin.flow.component.dependency.JavaScript and com.vaadin.flow.component.dependency.CssImport. 
 @JsModule("@polymer/paper-slider/paper-slider.js")
 // This is the extra js file being added
 // this js file will be finally included in an all-in-one bundle.js/main.js after webpack, therefore even the referencing
